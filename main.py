b39b5084f5ae8467de3dd66f754349c69b9be023
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2022-12-08 11:28:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+ad=input("oyuncunun adi")
+soyad=input("soyad")
+takim=input("takım adi")
+bilgiler=[ad,soyad,takim]
+print("data base kaydediliyor")
+print("oyuncu adi:{}\noyuncunun soyadi:{}\noyuncunun oynadigi takim:{}".format(bilgiler[0],bilgiler[1],bilgiler[2]))
